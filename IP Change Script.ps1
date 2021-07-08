c90277e0fcd53940db3f36a5a9f137c6087c9133
jon.kyer <>
2021-07-08 01:54:30 -0500

    Initial BrainStorm - Ed Boi

--- IP Change Script.ps1
+++ IP Change Script.ps1
@@ -7,9 +7,31 @@ Made by Edwin Brett, Please reach out via https://Edwinbrett.com if you have any
 
 Write-Host " IP Change Script
 ===================" -ForegroundColor Blue
-
 Write-Host "Searching for Adapters..." -ForegroundColor Yellow
 
-Get-NetAdapter -Name *
+
+$Adapters = (Get-NetAdapter).Name
+foreach ($interface in $Adapters) {
+  Write-Host "$interface found"
+  $response = Read-Host "Is this the adpater you want to work with? : Y or N"
+  if($response -eq 'Y' -or $response -eq 'y'){
+        Write-Host "We will work on this adapter:" $interface
+        #set ip 
+        #set DNS
+        #etcetc
+        break;
+
+  } elseif ($response -eq 'N' -or $response -eq 'n') {
+      Write-Host "Moving on to the next adapter"
+      
+  }
+  else{
+      Write-Host "LEARN TO FUCKING READ"
+  }
+}
+
+
 
 Write-Host "Select from previous list the adapter you would like to configure:"
+
+
