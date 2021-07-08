6cf30bcd136d63fe351d2a14e7471f3eab7f510f
Edwin Brett <office@edwinbrett.com>
2021-07-08 01:06:07 -0400

    Initial Commit

---
+++ IP Change Script.ps1
@@ -0,0 +1,15 @@
+<#
+This scripts purpose is to change IPs when configuring a VM for Private networking.
+Under normal conditions, it should select the secondary Ethernet adapter, and change the IP and DNS for it
+
+Made by Edwin Brett, Please reach out via https://Edwinbrett.com if you have any issues.
+#>
+
+Write-Host " IP Change Script
+===================" -ForegroundColor Blue
+
+Write-Host "Searching for Adapters..." -ForegroundColor Yellow
+
+Get-NetAdapter -Name *
+
+Write-Host "Select from previous list the adapter you would like to configure:"
