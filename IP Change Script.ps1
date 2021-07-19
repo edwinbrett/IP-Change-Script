7fff01569abedde82d00085fe7c17d3c8ff52269
jon.kyer <>
2021-07-19 18:30:35 -0500

    Updated script to fix logic bombs

--- IP Change Script.ps1
+++ IP Change Script.ps1
@@ -10,38 +10,35 @@ Write-Host " IP Change Script
 Write-Host "Searching for Adapters..." -ForegroundColor Yellow
 
 $Adapters = Get-NetAdapter
-foreach ($interface in $Adapters) {
-  Write-Host "$($interface.Name) found"
-  $response = Read-Host "Is this the adpater you want to work with? : Y or N"
-  if($response -eq 'Y' -or $response -eq 'y'){
-        Write-Host "We will work on this adapter:" $interface.Name
+if($null -ne $interface){
+    foreach ($interface in $Adapters) {
+        Write-Host "$($interface.Name) found"
+        $response = Read-Host "Is this the adpater you want to work with? : Y or N"
+        if($response -eq 'Y' -or $response -eq 'y'){
+                Write-Host "We will work on this adapter:" $interface.Name
         
-        $ipaddress = Read-Host "What would you like to set the IP to?"
-        $subnetmask = Read-Host "What would you like to set the Subnet mask to? Please submit the CIDR (/25, /32, etc.) with out the slash, aka '25'"
-        $gateway = Read-Host "What would you like to set the gateway to?"
-        $dnsserver = Read-Host "What would you like to set the DNS to?"
+                $ipaddress = Read-Host "What would you like to set the IP to?"
+                $subnetmask = Read-Host "What would you like to set the Subnet mask to? Please submit the CIDR (/25, /32, etc.) with out the slash, aka '25'"
+                $gateway = Read-Host "What would you like to set the gateway to?"
+                $dnsserver = Read-Host "What would you like to set the DNS to?"
+                Write-Host "Setting the Interface up now"
+                Remove-NetIPAddress -AddressFamily IPv4 -Confirm:$false -ErrorAction SilentlyContinue
+                Remove-NetRoute -AddressFamily IPv4 -Confirm:$false -ErrorAction SilentlyContinue
+                New-NetIPAddress -IPAddress $ipaddress -InterfaceAlias $($interface.ifAlias) -PrefixLength $subnetmask -DefaultGateway $gateway -AddressFamily IPv4
+                Start-Sleep -Seconds 2.5
+                Set-DnsClientServerAddress -ServerAddresses $dnsserver -InterfaceAlias $($interface.ifAlias)
+                Start-Sleep -Seconds 2.5
+                Write-Host "Ping Test to 1.1.1.1" 
+                ping 1.1.1.1
+                break;
 
-
-        Write-Host "Setting the Interface up now"
-
-        Remove-NetIPAddress -AddressFamily IPv4 -Confirm:$false -ErrorAction SilentlyContinue
-        Remove-NetRoute -AddressFamily IPv4 -Confirm:$false -ErrorAction SilentlyContinue
-        New-NetIPAddress -IPAddress $ipaddress -InterfaceAlias $($interface.ifAlias) -PrefixLength $subnetmask -DefaultGateway $gateway -AddressFamily IPv4
-        Start-Sleep -Seconds 2.5
-        Set-DnsClientServerAddress -ServerAddresses $dnsserver -InterfaceAlias $($interface.ifAlias)
-        Start-Sleep -Seconds 2.5
-        Write-Host "Ping Test to 1.1.1.1" 
-        ping 1.1.1.1
-        break;
-
-  } elseif ($response -eq 'N' -or $response -eq 'n' -and $_ -ne $null) {
-      Write-Host "Moving on to the next adapter"
+        } elseif ($response -eq 'N' -or $response -eq 'n') {
+              Write-Host "Checking for Another Adapter"
       
-  } elseif ($response -ne 'Y' -or $response -ne 'y' -or $response -ne 'N' -or $response -ne 'n' ){
-      Write-Host "LEARN TO READ"
-      break;
-  
-  } else{
-      Write-Host "There are no other adapters. Closing script"
-  }
+        } else{
+              Write-Host "LEARN TO READ"
+          break;
+        }    
+      }
+    Write-Host "There are no other adapters. Closing script"   
 }
